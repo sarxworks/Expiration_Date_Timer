f0738f54c1d689dc298f082b75b8b17badf09d70
sarxworks <sarxworks@gmail.com>
2025-06-22 08:17:56 -0400

    Fun code stuff

--- app/src/main/java/com/example/expirationdatetimer/MainActivity.kt
+++ app/src/main/java/com/example/expirationdatetimer/MainActivity.kt
@@ -4,6 +4,8 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Scaffold
@@ -11,37 +13,48 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import com.example.expirationdatetimer.ui.theme.ExpirationDateTimerTheme
 
 class MainActivity : ComponentActivity() {
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContent {
-            ExpirationDateTimerTheme {
-                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
-                    )
+            MessageCard(
+                        Message("Donte & Saloni", "Welcome to Android", "Look at My Code!")
+                        )
+
+
                 }
             }
         }
-    }
-}
+
+
+data class Message (val author: String, val body: String, val body2: String)
 
 @Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
+fun MessageCard(msg: Message){
+    Column {
+        Text(msg.author)
+        Text(msg.body)
+        Text(msg.body2)
+
+        Spacer(Modifier.padding(10.dp,10.dp))
+    }
+
+
+
+}
+
+// For Preview Screen (optional)
+@Preview
+@Composable
+fun MessageCardPreview() {
+    MessageCard(
+        Message("Donte & Saloni", "Welcome to Android", "Look at my Code!")
     )
-}
 
 
-@Composable
-fun GreetingPreview() {
-    ExpirationDateTimerTheme {
-        Greeting("Android")
     }
-}
